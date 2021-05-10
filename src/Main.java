abd7a451f7a0fa1142711e470a0acf710dbf1e2b
Mateusz <74718131+MateuszBilik@users.noreply.github.com>
2021-05-10 10:08:11 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,6 @@
-import jdk.swing.interop.SwingInterOpUtils;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Stream;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] numbers = reader.readLine().split(",");
-        int[] numbersInt = getInts(numbers);
-        List<String> response = new ArrayList<>();
-
-        for (int i = 0; i < numbers.length; i++) {
-            int firstNumber = numbersInt[i];
-            int nextNumber = numbersInt[i];
-            while (true){
-                if ((i + 1) < numbers.length) {
-                    if (numbersInt[i] == numbersInt[i + 1] || (numbersInt[i] + 1) == numbersInt[i + 1]) {
-                        i++;
-                        nextNumber = numbersInt[i];
-                    } else {
-                        response.add(firstNumber + "->" + nextNumber);
-                        break;
-                    }
-                } else {
-                    response.add(firstNumber + "->" + nextNumber);
-                    break;
-                }
-            }
-        }
-        System.out.println(response.toString());
+    public static void main(String[] args) {
     }
 
-    private static int[] getInts(String[] numbers) {
-        int[] numbersInt = new int[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-           numbersInt[i] = Integer.parseInt(numbers[i]);
-        }
-        return numbersInt;
-    }
 }
